0220961998fd49e6d80593c3441215d3c0f1727e
Caio Felipe <caio.braga.felipe@gmail.com>
2022-07-17 14:03:25 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -123,11 +123,11 @@ function mostraPlacar(){
   stroke(255)
   textAlign(CENTER);
   textSize(16);
-  fill(color(255,140,0));
+  fill(color(153,50,204));
   rect(150,10, 40, 20);
   fill(255);
   text(meusPontos,170,26);
-  fill(color(255,140,0))
+  fill(color(153,50,204))
   rect(450,10, 40, 20);
   fill(255);
   text(pontosOponente,470,26);
